431a9760e55a3c107889e7999dd26ef2e1602467
LinkDown <35885366+LinkDownZ@users.noreply.github.com>
2025-12-08 18:26:14 +0800

    Update EmbyPremiere.js

--- EmbyPremiere/EmbyPremiere.js
+++ EmbyPremiere/EmbyPremiere.js
@@ -1,7 +1,6 @@
 if ($request.url.indexOf('mb3admin.com/admin/service/registration/validateDevice') != -1) {
     if($response.status!=200){
         //$notification.post("Emby Premiere 已激活", "", "");
-        $notification.post("hello world!", "", "");
         $done({status: 200, headers: $response.headers, body: '{"cacheExpirationDays":999,"resultCode":"GOOD","message":"Device Valid"}' })
     }else{
         $done({})
